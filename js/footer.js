d67df951937b047a5ac19c6456c2b7ccef0cfe20
juan manuel rolando <juanmanuelrolando@gmail.com>
2023-06-04 22:28:58 +0000

    Update 9 files

---
+++ js/footer.js
@@ -0,0 +1,41 @@
+function cargarFooter() {
+
+let contenedorFooter = document.getElementById('Footer')
+
+contenedorFooter .innerHTML = `
+<div>
+    <ul class="footer-comlunas">
+      <li class="footer-titulos">LASER LED</li>
+      <br>
+      <li class="footer-text">Direccion</li>
+      <br>
+      <li class="footer-text">Telefono</li>
+      <br>
+      <li class="footer-text">Mail</li>
+    </ul>
+  </div>
+  <div>
+    <ul class="footer-comlunas">
+      <li class="footer-titulos">LASER LED</li>
+      <br>
+      <li class="footer-text">Direccion</li>
+      <br>
+      <li class="footer-text">Telefono</li>
+      <br>
+      <li class="footer-text">Mail</li>
+    </ul>
+  </div> 
+  <div>
+  <ul class="footer-comlunas">
+    <li class="footer-titulos">LASER LED</li>
+    <br>
+    <li class="footer-text">Direccion</li>
+    <br>
+    <li class="footer-text">Telefono</li>
+    <br>
+    <li class="footer-text">Mail</li>
+  </ul>
+</div> 
+`
+} 
+        cargarFooter()
