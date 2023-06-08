70764687fab08586a6a04a86527e231a88ffd52f
juan manuel rolando <juanmanuelrolando@gmail.com>
2023-06-08 09:58:30 +0000

    Update func.js

--- js/func.js
+++ js/func.js
@@ -50,25 +50,22 @@ productos.forEach((x) => {
             })
         }
         localStorage.setItem('productosSelecionados', JSON.stringify(carrito))
+        Toastify({
+            text: "This is a toast",
+            duration: 3000,
+            // destination: "https://github.com/apvarun/toastify-js",
+            // newWindow: true,
+            // close: true,
+            // gravity: "top", // `top` or `bottom`
+            // position: "left", // `left`, `center` or `right`
+            // stopOnFocus: true, // Prevents dismissing of toast on hover
+            // style: {
+            //   background: "linear-gradient(to right, #00b09b, #96c93d)",
+            // },
+            // onClick: function(){} // Callback after click
+          }).showToast();
     })
 
 })
 
 
-const prodAgregado = document.querySelector('.prodAgregado')
-prodAgregado.addEventListener('click', () => {
-    Toastify({
-        text: "This is a toast",
-        duration: 3000,
-        // destination: "https://github.com/apvarun/toastify-js",
-        // newWindow: true,
-        // close: true,
-        // gravity: "top", // `top` or `bottom`
-        // position: "left", // `left`, `center` or `right`
-        // stopOnFocus: true, // Prevents dismissing of toast on hover
-        // style: {
-        //   background: "linear-gradient(to right, #00b09b, #96c93d)",
-        // },
-        // onClick: function(){} // Callback after click
-      }).showToast();
-    })
