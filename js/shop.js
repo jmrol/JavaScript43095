aae9c6106cb53ac9fa829ea84a05991424bafc97
juan manuel rolando <juanmanuelrolando@gmail.com>
2023-06-20 02:10:12 +0000

    Update settings.json and shop.js

--- js/shop.js
+++ js/shop.js
@@ -1,23 +1,23 @@
 
-let productos = []
+var productos = []
 
   fetch('/data.json')
   .then(res => res.json())
 
     .then((data) => {
         cargarProductos(data)
-    
+        
     })
  
-  
-   
+
+
 
     
     cargarProductos = (data) => {
       
     var carrito = []
     productos = data;
-    
+    // console.log (data)
     let contenedor = document.getElementById('card-productos')
     
     productos.forEach((x) => {
@@ -74,26 +74,75 @@ let productos = []
 
 }
 
+
+fetch('/data.json')
+.then(res => res.json())
+
+  .then((data) => {
+    filtrarProductos(data)
+
+  })
+
+
 function filtrarProductos(data) {
+
     let product = data;
     let textoBusqueda = document.getElementById("textoBusqueda").value;
     let contenido = "";
-
+    console.log (data)
     product = textoBusqueda ? product.filter(item => item.nombre.toUpperCase().includes(textoBusqueda.toUpperCase())) : product;
 
     if (product.length > 0) {
         product.forEach(x => {
-            contenido += `<div class="col-md-3 mb-5">
-            <a href="ver-producto.html" onclick="verProducto(${x.id})" class="text-decoration-none">
-                <div class="card text-center border border-0">
-                    <img src="${x.imagen}" class="card-img-top" alt="${x.nombre}">
-                    <div class="card-body">
-                        <p class="card-text text-primary"><b>$${x.precio}</b></p>
-                        <p class="text-secondary">${x.nombre}</p>
-                    </div>
+            let card = document.createElement('div')
+            card.innerHTML = `
+                <div class="cards-prod">
+                <div class="contenedor-imagen">
+                <img src="${x.imagen}" alt=""  id= "img-prod-card" >
                 </div>
-            </a>
-            </div>`;
+            <div class="card-body" id= "card-b">
+            <p class="">Precio: $${x.precio}</p>
+            <p class="">${x.nombre}</p>
+        
+            </div>
+            </div>
+            `
+            contenedor.append(card)
+        
+            let comprar = document.createElement('button')
+            comprar.id = 'prodAgregado'
+            comprar.className = 'prodAgregado'
+            comprar.innerText = 'Comprar'
+            card.appendChild(comprar)
+        
+            comprar.addEventListener('click', () => {
+                const repeat = carrito.some((repeatProduct) => repeatProduct.id === x.id)
+        
+                      if (repeat) {
+                    carrito.map((prod) => {
+                        if (prod.id === x.id) {
+                            prod.cantidad++
+                        }
+                    })
+                } else {
+                    carrito.push({
+                        id: x.id,
+                        nombre: x.nombre,
+                        precio: x.precio,
+                        imag: x.imagen,
+                        cantidad: x.cantidad,
+                    })
+                }
+               
+              localStorage.setItem('productosSelecionados', JSON.stringify(carrito))
+        
+        
+        
+              alertaProductoAgregado()
+              contadorCarrito(carrito)
+        
+             })
+        
         });
     } else {
         contenido += `<div class="alert alert-danger text-center" role="alert">No se encontraron productos por el término de búsqueda!</div>`;
