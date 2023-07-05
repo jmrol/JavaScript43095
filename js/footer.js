9d9a53143f935ea497d663bdac326e5e090e0da1
juan manuel rolando <juanmanuelrolando@gmail.com>
2023-07-05 10:31:53 +0000

    Update 6 files

--- js/footer.js
+++ js/footer.js
@@ -7,33 +7,33 @@ contenedorFooter .innerHTML = `
     <ul class="footer-comlunas">
       <li class="footer-titulos">LASER LED</li>
       <br>
-      <li class="footer-text">Direccion</li>
+      <li class="footer-text">Direccion: Paz 1050</li>
       <br>
-      <li class="footer-text">Telefono</li>
+      <li class="footer-text">Telefono: 2486541236</li>
       <br>
-      <li class="footer-text">Mail</li>
+      <li class="footer-text">Mail: Laserled@sdasd</li>
     </ul>
   </div>
   <div>
     <ul class="footer-comlunas">
-      <li class="footer-titulos">LASER LED</li>
+      <li class="footer-titulos">SHOP</li>
       <br>
-      <li class="footer-text">Direccion</li>
+      <li class="footer-text">Paneles</li>
       <br>
-      <li class="footer-text">Telefono</li>
+      <li class="footer-text">Fogones</li>
       <br>
-      <li class="footer-text">Mail</li>
+      <li class="footer-text">Esculturas</li>
     </ul>
   </div> 
   <div>
   <ul class="footer-comlunas">
-    <li class="footer-titulos">LASER LED</li>
+    <li class="footer-titulos">Comercial</li>
     <br>
-    <li class="footer-text">Direccion</li>
+    <li class="footer-text">Envios</li>
     <br>
-    <li class="footer-text">Telefono</li>
+    <li class="footer-text">Opciones de acabado</li>
     <br>
-    <li class="footer-text">Mail</li>
+    <li class="footer-text">Proyectos especiales</li>
   </ul>
 </div> 
 `
